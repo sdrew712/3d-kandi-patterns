37080d26ba846cc848810fca6624276ebc770ea3
Sarah Drew <drew.sarah.e@gmail.com>
2024-02-24 00:57:43 -0700

    make square component

--- src/app/cube.tsx
+++ src/app/cube.tsx
@@ -1,21 +1,41 @@
+import * as THREE from "three";
 import styles from "./page.module.css";
 import { Canvas } from "@react-three/fiber";
 
 export default function Cube() {
   return (
     <div className={styles.canvasContainer}>
-      <Canvas>
+      <Canvas resize={{ scroll: true }}>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <mesh position={[2, 2, 0]}>
-          <boxGeometry args={[2, 2, 0]} />
-          <meshStandardMaterial color="orange" />
-        </mesh>
-        <mesh>
-          <boxGeometry args={[2, 2, 0]} />
-          <meshStandardMaterial color="red" />
-        </mesh>
+
+        <Square position={-1} color="red" />
       </Canvas>
     </div>
+  );
+}
+
+function Square({ position, color }: { position: number; color: string }) {
+  const positionArray: [x: number, y: number, z: number] = [
+    position,
+    position,
+    position,
+  ];
+
+  return (
+    <>
+      <mesh position={positionArray}>
+        <boxGeometry args={[2, 2, 0]} />
+        <meshStandardMaterial color={color} />
+
+        <lineSegments>
+          <edgesGeometry
+            attach="geometry"
+            args={[new THREE.BoxGeometry(2, 2, 0)]}
+          />
+          <lineBasicMaterial attach="material" color="black" />
+        </lineSegments>
+      </mesh>
+    </>
   );
 }
