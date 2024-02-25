059609746eaa0c5a5b6fcd8091168306e8a3d5f9
Sarah Drew <drew.sarah.e@gmail.com>
2024-02-24 23:24:55 -0700

    orbit progress!

--- src/app/Orbit.tsx
+++ src/app/Orbit.tsx
@@ -14,68 +14,44 @@ export default function Orbit() {
         <OrbitControls />;
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        {/* outline */}
-        <Square x={0} y={0} color="gray" />
-        <Square x={2} y={0} color="gray" />
-        <Square x={4} y={-2} color="gray" />
-        <Square x={6} y={-4} color="gray" />
-        <Square x={8} y={-2} color="gray" />
-        <Square x={10} y={0} color="gray" />
-        <Square x={12} y={0} color="gray" />
-        <Square x={12} y={-2} color="gray" />
-        <Square x={12} y={-4} color="gray" />
-        <Square x={10} y={-6} color="gray" />
-        <Square x={8} y={-8} color="gray" />
-        <Square x={10} y={-10} color="gray" />
-        <Square x={12} y={-12} color="gray" />
-        <Square x={12} y={-14} color="gray" />
-        <Square x={12} y={-16} color="gray" />
-        <Square x={10} y={-18} color="gray" />
-        <Square x={8} y={-20} color="gray" />
-        <Square x={8} y={-18} color="gray" />
-        <Square x={8} y={-16} color="gray" />
-        <Square x={8} y={-14} color="gray" />
-        <Square x={6} y={-12} color="gray" />
-        <Square x={4} y={-14} color="gray" />
-        <Square x={4} y={-16} color="gray" />
-        <Square x={4} y={-18} color="gray" />
-        <Square x={4} y={-20} color="gray" />
-        <Square x={4} y={-22} color="gray" />
-        <Square x={2} y={-20} color="gray" />
-        <Square x={0} y={-18} color="gray" />
-        <Square x={0} y={-16} color="gray" />
-        <Square x={0} y={-14} color="gray" />
-        <Square x={0} y={-12} color="gray" />
-        <Square x={2} y={-10} color="gray" />
-        <Square x={4} y={-8} color="gray" />
-        <Square x={2} y={-6} color="gray" />
-        <Square x={0} y={-4} color="gray" />
-        <Square x={0} y={-2} color="gray" />
-        {/* fill */}
-        <Square x={2} y={-2} color="#e0b3fc" />
-        <Square x={4} y={-4} color="#e0b3fc" />
-        <Square x={6} y={-6} color="#e0b3fc" />
-        <Square x={8} y={-4} color="#e0b3fc" />
-        <Square x={10} y={-2} color="#e0b3fc" />
-        <Square x={10} y={-4} color="#e0b3fc" />
-        <Square x={8} y={-6} color="#e0b3fc" />
-        <Square x={6} y={-8} color="#e0b3fc" />
-        <Square x={8} y={-10} color="#e0b3fc" />
-        <Square x={10} y={-12} color="#e0b3fc" />
-        <Square x={10} y={-14} color="#e0b3fc" />
-        <Square x={10} y={-16} color="#e0b3fc" />
-        <Square x={8} y={-12} color="#e0b3fc" />
-        <Square x={6} y={-10} color="#e0b3fc" />
-        <Square x={4} y={-12} color="#e0b3fc" />
-        <Square x={2} y={-14} color="#e0b3fc" />
-        <Square x={2} y={-16} color="#e0b3fc" />
-        <Square x={2} y={-18} color="#e0b3fc" />
-        <Square x={2} y={-12} color="#e0b3fc" />
-        <Square x={4} y={-10} color="#e0b3fc" />
-        <Square x={4} y={-6} color="#e0b3fc" />
-        <Square x={2} y={-4} color="#e0b3fc" />
+        {/* orbit */}
+        <MainSide />
       </Canvas>
     </div>
+  );
+}
+
+function MainSide() {
+  return (
+    <>
+      <Square x={-8} y={4} color="#D288FF" />
+      <Square x={-6} y={4} color="#D288FF" />
+      <Square x={0} y={4} color="#D288FF" />
+      <Square x={2} y={4} color="#D288FF" />
+
+      <Square x={-11} y={2} color="#05E6FA" />
+      <Square x={-9} y={2} color="#05E6FA" />
+      <Square x={-7} y={2} color="#05E6FA" />
+      <Square x={-5} y={2} color="#05E6FA" />
+      <Square x={-3} y={2} color="#05E6FA" />
+      <Square x={-1} y={2} color="#D288FF" />
+      <Square x={1} y={2} color="#D288FF" />
+      <Square x={3} y={2} color="#05E6FA" />
+      <Square x={5} y={2} color="#05E6FA" />
+
+      <Square x={8} y={0} color="#D288FF" />
+      <Square x={6} y={0} color="#05E6FA" />
+      <Square x={4} y={0} color="#05E6FA" />
+      <Square x={2} y={0} color="#D288FF" />
+      <Square x={0} y={0} color="#05E6FA" />
+      <Square x={-2} y={0} color="#05E6FA" />
+      <Square x={-4} y={0} color="#05E6FA" />
+      <Square x={-6} y={0} color="#05E6FA" />
+      <Square x={-8} y={0} color="#05E6FA" />
+      <Square x={-10} y={0} color="#05E6FA" />
+      <Square x={-12} y={0} color="#05E6FA" />
+      <Square x={-14} y={0} color="#D288FF" />
+    </>
   );
 }
 
@@ -91,7 +67,7 @@ function Square({ x, y, color }: { x: number; y: number; color: string }) {
         <lineSegments>
           <edgesGeometry
             attach="geometry"
-            args={[new THREE.BoxGeometry(2, 2, 0)]}
+            args={[new THREE.BoxGeometry(2, 2, 2)]}
           />
           <lineBasicMaterial attach="material" color="black" />
         </lineSegments>
