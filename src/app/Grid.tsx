7354847e691b7b51d21468256284af0cc45e56f0
Sarah Drew <drew.sarah.e@gmail.com>
2024-06-23 18:23:09 -0700

    grid squares only at even numbers

--- src/app/Grid.tsx
+++ src/app/Grid.tsx
@@ -6,9 +6,9 @@ export function Grid({ plane }: { plane: Pattern[0] }) {
 
   return (
     <group>
-      {Array.from({ length: 50 }, (_, i) =>
-        Array.from({ length: 50 }, (_, j) => (
-          <Square key={`${i} ${j}`} x={i - patternStart} y={j - patternStart} />
+      {Array.from({ length: 10 }, (_, i) =>
+        Array.from({ length: 10 }, (_, j) => (
+          <Square key={`${i} ${j}`} x={i * 2} y={j * 2} />
         ))
       )}
     </group>
