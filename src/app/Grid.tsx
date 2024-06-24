caed6a9fbfbe2ad8c3d79101ba09844d3e63d2ac
Sarah Drew <drew.sarah.e@gmail.com>
2024-06-23 18:40:26 -0700

    center grid, kind of

--- src/app/Grid.tsx
+++ src/app/Grid.tsx
@@ -2,15 +2,25 @@ import { Pattern } from "@/types";
 import Square from "./Square";
 
 export function Grid({ plane }: { plane: Pattern[0] }) {
-  const patternStart = plane.beads.length / 2;
+  const xPatternStart = plane.beads[0].x;
+  const yPatternStart = plane.beads[0].y;
+
+  const xGridStart = xPatternStart - 4;
+  const yGridStart = yPatternStart + 4;
 
   return (
-    <group>
-      {Array.from({ length: 10 }, (_, i) =>
-        Array.from({ length: 10 }, (_, j) => (
-          <Square key={`${i} ${j}`} x={i * 2} y={j * 2} />
-        ))
-      )}
-    </group>
+    <>
+      <group>
+        {Array.from({ length: 14 }, (_, i) =>
+          Array.from({ length: 14 }, (_, j) => (
+            <Square
+              key={`${i} ${j}`}
+              x={xGridStart + i * 2}
+              y={yGridStart - j * 2}
+            />
+          ))
+        )}
+      </group>
+    </>
   );
 }
