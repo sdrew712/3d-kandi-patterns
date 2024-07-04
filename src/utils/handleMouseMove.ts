04c391687cada13d2df1a6c65cd0a845458a1495
Sarah Drew <drew.sarah.e@gmail.com>
2024-07-04 16:24:07 -0700

    remove negative zeros in handleMouseMove

--- src/utils/handleMouseMove.ts
+++ src/utils/handleMouseMove.ts
@@ -18,10 +18,25 @@ export function handleMouseMove({
   intersects.forEach((intersection) => {
     const coords = intersection.point;
 
-    setMousePosition({
+    const roundedCoords = {
       x: coords.x > 0 ? Math.floor(coords.x) : Math.ceil(coords.x),
       y: coords.y > 0 ? Math.floor(coords.y) : Math.ceil(coords.y),
       z: coords.z > 0 ? Math.floor(coords.z) : Math.ceil(coords.z),
+    };
+    if (Object.is(roundedCoords.x, -0)) {
+      roundedCoords.x = 0;
+    }
+
+    if (Object.is(roundedCoords.y, -0)) {
+      roundedCoords.y = 0;
+    }
+
+    if (Object.is(roundedCoords.z, -0)) {
+      roundedCoords.z = 0;
+    }
+
+    setMousePosition({
+      ...roundedCoords,
     });
   });
 }
